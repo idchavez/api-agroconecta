31a716aea6247940ad7b51de1ec934e39431f955
DanielVega825 <danielvegalinares@hotmail.com>
2026-05-25 12:33:25 -0500

    se crea la rama develop

--- src/main/java/com/apiagroconecta/api_agroconecta/model/Usuario.java
+++ src/main/java/com/apiagroconecta/api_agroconecta/model/Usuario.java
@@ -1,10 +1,28 @@
 package com.apiagroconecta.api_agroconecta.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "usuarios")
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombres;
+
+    @Column(nullable = false)
+    private String telefono;
+
+    @Column(unique = true, nullable = false)
+    private String correo;
+
+    @Column(nullable = false)
+    private String password;
+
+
 }
